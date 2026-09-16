0da35caf74e94c084fd8eb10a33ee71e7df348cd
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-09-16 01:12:16 +0100

    Mob spawn (#6)
    * Update game resolution
    
    * Add spawn path
    
    * Add main script
    
    * Spawn mobs

---
+++ scripts/main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+
+	var mob = mob_scene.instantiate()
+	var player_position = $Player.position
+	mob.initialise(mob_spawn_location.position, player_position)
+
+	add_child(mob)
