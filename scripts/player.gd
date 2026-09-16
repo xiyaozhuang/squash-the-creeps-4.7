e118714b7026c9ba12b94049d37ec827b83725ae
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-09-17 00:32:29 +0100

    Add player jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 14
 @export var fall_acceleration = 75
+@export var jump_impulse = 20
 
 var target_velocity = Vector3.ZERO
 
@@ -33,4 +34,8 @@ func _physics_process(delta: float) -> void:
 		target_velocity.y = target_velocity.y - fall_acceleration * delta
 
 	velocity = target_velocity
+
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y = jump_impulse
+
 	move_and_slide()
