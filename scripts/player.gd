7316e4c6917a5a1fe90e89875e1e74f13bee584a
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-09-17 01:32:37 +0100

    Player death (#8)
    * Add player hitbox
    
    * Set collision properties
    
    * Add player death
    
    * Add game ending

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal hit
+
 @export var speed = 14
 @export var fall_acceleration = 75
 @export var jump_impulse = 20
@@ -55,3 +57,12 @@ func _physics_process(delta: float) -> void:
 				break
 
 	move_and_slide()
+
+
+func die() -> void:
+	hit.emit()
+	queue_free()
+
+
+func _on_mob_detector_body_entered(_body: Node3D) -> void:
+	die()
