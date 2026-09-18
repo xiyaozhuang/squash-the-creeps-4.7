bf474055d654379cdde781aa54291d542cf47f52
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-09-18 22:15:30 +0100

    Character animation (#10)
    * Add player animation
    
    * Increase player animation speed when moving
    
    * Add arc to jump animation
    
    * Copy float animation to mob scene
    
    * Scale mob animation speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -58,6 +58,14 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+	if direction != Vector3.ZERO:
+		$AnimationPlayer.speed_scale = 4
+
+	else:
+		$AnimationPlayer.speed_scale = 1
+
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
+
 
 func die() -> void:
 	hit.emit()
